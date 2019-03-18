53390f32242763b8d50ef6bc382133a1454564d2
dharsan05 <dharsanrajvir@gmail.com>
2019-03-18 22:24:32 +0530

    Added array program

---
+++ src/org/dharsan/Dharsan_01.java
@@ -0,0 +1,21 @@
+package org.dharsan;
+
+public class Dharsan_01 {
+	
+
+	public static void main(String[] args) {
+		int arr[]=new int[5];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		arr[3]=40;
+		arr[4]=50;
+		System.out.println(arr.length);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+		
+		
+	}
+	}
